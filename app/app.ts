596c9ec1e885408be9efd91604d49ca65e9bf703
jefferson.dev <jefferson.dev@grupointegrado.br>
2024-07-16 13:13:42 -0300

    alteraçoes do curso

---
+++ app/app.ts
@@ -0,0 +1,5 @@
+import { Negociacao } from './models/negociacao.js';
+
+const negociacao = new Negociacao(new Date(),3,4);
+console.log("xxx",negociacao.quantidade);
+
---
+++ dist/js/app.js
@@ -0,0 +1,3 @@
+import { Negociacao } from './models/negociacao.js';
+const negociacao = new Negociacao(new Date(), 3, 4);
+console.log("xxx", negociacao.quantidade);
